18c150970bdfb90350d324c925c70f3b98855ca3
c0r73x <c0r73x@gmail.com>
2025-03-24 11:30:05 +0100

    fixed shift for gamemode

--- keymap.c
+++ keymap.c
@@ -64,20 +64,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_TRNS,
         KC_TRNS, KC_T,    KC_Q,    KC_W,    KC_E,    KC_R, KC_TRNS,
         KC_TRNS, KC_G,    KC_A,    KC_S,    KC_D,    KC_F,
-        KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_TRNS,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_SPACE, KC_TRNS, KC_TRNS,
         // right hand
         KC_TRNS, KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    SE_PLUS,
         KC_TRNS, KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    SE_ARNG,
                  KC_H,    KC_J,    KC_K,     KC_L,    SE_ODIA, SE_ADIA,
-        KC_TRNS, KC_N,    KC_M,    KC_COMMA, KC_DOT,  SE_MINS, KC_TRNS,
+        KC_TRNS, KC_N,    KC_M,    KC_COMMA, KC_DOT,  SE_MINS, KC_RSHFT,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_ENTER
     ),
 
     // [POE] = KEYMAP(
